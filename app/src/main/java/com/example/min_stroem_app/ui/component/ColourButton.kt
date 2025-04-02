ab0cb5c38c9df16427eba2d87ba00e956cc96447
bananpalme <77321863+bananpalme@users.noreply.github.com>
2025-04-02 11:03:34 +0200

    created colourbutton

--- app/src/main/java/com/example/min_stroem_app/ui/component/ColourButton.kt
+++ app/src/main/java/com/example/min_stroem_app/ui/component/ColourButton.kt
@@ -1,19 +1,9 @@
 package com.example.min_stroem_app.ui.component
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 
 
 @Composable
